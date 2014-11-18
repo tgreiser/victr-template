9c70b0de0f9ceccc0979b6e5ac5926bee49dccbf
Tim Greiser <tgreiser@gmail.com>
2014-11-18 12:01:04 -0800

    playing with paths to try to get init running only once

--- site/http.go
+++ site/http.go
@@ -2,18 +2,16 @@ package theme
 
 import (
   "net/http"
-  "github.com/tgreiser/victr/app"
+  _ "github.com/tgreiser/victr/app"
   "github.com/stretchr/goweb"
   "github.com/stretchr/goweb/context"
   "github.com/stretchr/goweb/handlers"
 )
 
 func init() {
-  app.Init()
-
   handler := handlers.NewHttpHandler(goweb.CodecService)
   handler.Map("GET", "/app", func (c context.Context) error {
     return goweb.Respond.With(c, 200, []byte("Hey app!"))
   })
-  http.Handle("/app", handler)
+  http.Handle("/app/", handler)
 }
